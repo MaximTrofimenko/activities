7e8d175cbfbe38de92c21219d844c3f6dedb1b6b
Maxim Trofimenko <maxim.trofimenko@corp.codeinside.ru>
2024-01-22 21:38:58 +0300

    new endpoint - postActivity

---
+++ src/main/kotlin/com/tmg/activities/exceptions/NotFoundException.kt
@@ -0,0 +1,10 @@
+package com.tmg.activities.exceptions
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+class NotFoundException(message: String) : RuntimeException(message) {
+}
+
+
--- src/main/kotlin/com/tmg/activities/integrationdb/dao/ActivitiesDao.kt
+++ src/main/kotlin/com/tmg/activities/integrationdb/dao/ActivitiesDao.kt
@@ -4,5 +4,4 @@ import com.tmg.activities.integrationdb.entity.ActivityEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
-interface ActivitiesDao : JpaRepository<ActivityEntity, UUID> {
-}
+interface ActivitiesDao : JpaRepository<ActivityEntity, UUID>
