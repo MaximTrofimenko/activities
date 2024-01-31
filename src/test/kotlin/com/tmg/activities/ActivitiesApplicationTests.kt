f801a925347fb9aab4a778b0dc7bed09274aa84b
Maxim Trofimenko <maxim.trofimenko@corp.codeinside.ru>
2024-01-31 18:44:19 +0300

    with and apply example

--- src/test/kotlin/com/tmg/activities/ActivitiesApplicationTests.kt
+++ src/test/kotlin/com/tmg/activities/ActivitiesApplicationTests.kt
@@ -2,11 +2,14 @@ package com.tmg.activities
 
 import com.tmg.activities.integrationdb.domain.ActivityRsDto
 import com.tmg.activities.integrationdb.domain.ActivityType
+import com.tmg.activities.integrationdb.entity.ActivityEntity
 import com.tmg.activities.util.CaseInsensitiveFileComparator
+import com.tmg.activities.util.alphabet
 import com.tmg.activities.util.getPrettyAvgPace
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.time.Instant
+import java.util.UUID
 
 class ActivitiesApplicationTests {
 
@@ -38,27 +41,35 @@ class ActivitiesApplicationTests {
             id = null
         )
 
-        val toList = listOf(1, 2, 3, 4).asSequence().map { it + 3 }.filter { it > 5 }.toList()
+        //apply можно применять только к уже созданному объекту(!), например если на нужно поменять поле
+        //apply меняет УЖЕ созданный объект
+        activityRsDto2.apply { kindOfRun = "asdfg" }
 
+        val activity = ActivityEntity(
+            id = UUID.randomUUID(),
+            distance = 10000,
+            totalTime = 1000,
+            date = Instant.now(),
+            type = ActivityType.SKI_CLASSIC
+        ).apply { id = UUID.randomUUID() }
+
+        val toList = listOf(1, 2, 3, 4).asSequence().map { it + 3 }.filter { it > 5 }.toList()
 
         val listOf = listOf(activityRsDto1, activityRsDto2)
 
         val filter = listOf
-            .map {
-                it.distance
-
-            }
+            .map { it.distance }
             .filter { it == 1000 }
-
 
         val compare = CaseInsensitiveFileComparator.compare(activityRsDto1, activityRsDto2)
         println(compare)
 
-
         val objects = ArrayList<Int>()
-        val integers = objects.stream()
+        val integers = objects.asSequence()
             .map { it + 5 }
             .filter { it > 5 }
             .toList()
+
+        println(alphabet())
     }
 }
